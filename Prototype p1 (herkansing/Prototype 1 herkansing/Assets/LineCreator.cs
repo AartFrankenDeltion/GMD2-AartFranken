759b2a2386d670dae19b21e0a8aac2077e86e111
Aart Franken <97046361@st.deltion.nl>
2018-02-02 16:57:27 +0100

    Prototype herkansing p1

---
+++ Prototype p1 (herkansing/Prototype 1 herkansing/Assets/LineCreator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreator : MonoBehaviour
+{
+    public LineList lijnlijstje;
+
+    Line activeLine;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject LineGO = Instantiate(lijnlijstje.selectedLine);
+            activeLine = LineGO.GetComponent<Line>();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+        }
+
+        if (activeLine != null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            activeLine.UpdateLine(mousePos);
+        }
+
+    }
+
+}
---
+++ Prototype p1 (herkansing/Prototype 1 herkansing/Assets/LineList.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineList : MonoBehaviour {
+
+    public List<GameObject> lijnList = new List<GameObject>();
+    public GameObject selectedLine;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButton("Z"))
+        {
+            selectedLine = lijnList[1];
+        }
+        else if (Input.GetButton("X"))
+        {
+            selectedLine = lijnList[2];
+        }
+        else if (Input.GetButton("C"))
+        {
+            selectedLine = lijnList[3];
+        }
+        else
+        {
+            selectedLine = lijnList[0];
+        }
+
+    }
+}
